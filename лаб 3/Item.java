3d4bc3b58235577677e56f32a75bc5f6096d872f
IhorTokariev <145541061+IhorTokariev@users.noreply.github.com>
2024-11-23 22:01:11 +0200

    Encapsulation of fields
    Fields changed from public to private to comply with the encapsulation principle.

--- лаб 3/Item.java
+++ лаб 3/Item.java
@@ -2,11 +2,10 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private String name;
+    private int sellIn;
+    private int quality;
 
-    public int sellIn;
-
-    public int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
