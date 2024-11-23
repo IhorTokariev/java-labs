25ac15b2379ada46ceb750f2f7fc8c244d9ffa07
IhorTokariev <145541061+IhorTokariev@users.noreply.github.com>
2024-11-23 22:05:57 +0200

    Adding validation in the constructor
    Added validations for input data in the constructor.
    The name cannot be empty or null. sellin cannot be negative.
    quality should be within [0, 50].

--- лаб 3/Item.java
+++ лаб 3/Item.java
@@ -1,17 +1,25 @@
 package com.gildedrose;
 
 public class Item {
-
     private String name;
     private int sellIn;
     private int quality;
 
-
     public Item(String name, int sellIn, int quality) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        if (sellIn < 0) {
+            throw new IllegalArgumentException("SellIn cannot be negative");
+        }
+        if (quality < 0 || quality > 50) {
+            throw new IllegalArgumentException("Quality must be between 0 and 50");
+        }
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
+
 
    @Override
    public String toString() {
