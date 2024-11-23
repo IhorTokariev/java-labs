33446fdca395a2a9816857f126d402bfe30a1d99
IhorTokariev <145541061+IhorTokariev@users.noreply.github.com>
2024-11-23 21:17:00 +0200

    The logic of updating each item
    The logic is divided into separate methods:
    updateAgedBrie for "Aged Brie".
    updateBackstagePass for "Backstage passes".
    updateRegularItem for regular items.
    Added isSulfuras method:
    Special items no longer require a nested check.
    The logic for reducing sellIn is placed in the decreaseSellIn method.
    Expired items are handled separately in handleExpiredItem

--- лаб 3/GildedRose.java
+++ лаб 3/GildedRose.java
@@ -9,37 +9,29 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-    updateItem(item); 
-}
+            updateItem(item);
+        }
+    }
 
-            
-            
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    private void updateItem(Item item) {
+        if (isSulfuras(item)) {
+            return;
+        }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        if (isAgedBrie(item)) {
+            updateAgedBrie(item);
+        } else if (isBackstagePass(item)) {
+            updateBackstagePass(item);
+        } else {
+            updateRegularItem(item);
+        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+        decreaseSellIn(item);
+
+        if (item.sellIn < 0) {
+            handleExpiredItem(item);
+        }
+    }
 
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
