9c650a25e57b7987825cd3e85fe6c1977cd65b3d
IhorTokariev <145541061+IhorTokariev@users.noreply.github.com>
2024-11-23 22:09:27 +0200

    Adding getters and setters
    Додано геттери та сеттери для кожного поля

--- лаб 3/Item.java
+++ лаб 3/Item.java
@@ -1,5 +1,3 @@
-package com.gildedrose;
-
 public class Item {
     private String name;
     private int sellIn;
@@ -20,6 +18,32 @@ public class Item {
         this.quality = quality;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        if (quality < 0 || quality > 50) {
+            throw new IllegalArgumentException("Quality must be between 0 and 50");
+        }
+        this.quality = quality;
+    }
 
    @Override
    public String toString() {
