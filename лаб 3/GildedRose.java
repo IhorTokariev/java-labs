29e0096b75d2a0f12a4cc670c8b05541fd14912d
IhorTokariev <145541061+IhorTokariev@users.noreply.github.com>
2024-11-23 21:32:45 +0200

    Final GildedRose commit
    Moved the handling of expired items to the handleExpiredItem method

--- лаб 3/GildedRose.java
+++ лаб 3/GildedRose.java
@@ -63,23 +63,15 @@ class GildedRose {
         decreaseQuality(item);
     }
 
-   if (items[i].sellIn < 0) {
-    if (!items[i].name.equals("Aged Brie")) {
-        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items[i].quality > 0) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
+    private void handleExpiredItem(Item item) {
+        if (isAgedBrie(item)) {
+            increaseQuality(item);
+        } else if (isBackstagePass(item)) {
+            item.quality = 0;
         } else {
-            items[i].quality = items[i].quality - items[i].quality;
-        }
-    } else {
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+            decreaseQuality(item);
         }
     }
-}
 
     private void decreaseSellIn(Item item) {
         item.sellIn -= 1;
